6d77627653293d8f74e966dfabc4a0c79bc51062
187J3X1-114514 <smjwoainni@qq.com>
2024-10-09 21:04:55 +0800

    修复在学校打不开的问题（待测试😅）

--- src/core/font.ts
+++ src/core/font.ts
@@ -1,40 +1,28 @@
-import FontFaceObserver from 'fontfaceobserver';
 export const fontPath = "assets/font/font.woff"
 export const fontPathPhi = "assets/font/font_phi.woff"
 export const OutGameFontName = "Font,phi"
 export const InGameFontName = "phi,Font"
-const style =
-    `
-@font-face {
-    font-family: Font;
-    src:
-        url("${fontPath}2") format("woff2"),
 
-}
-@font-face {
-    font-family: phi;
-    src:
-        url("${fontPathPhi}2") format("woff2"),
-}
-@font-face {
-  font-display: swap;
-  font-family: 'Fluent UI Emoji Flat';
-  font-style: normal;
-  font-weight: normal;
-  src: url('assets/font/emoji.woff2') format('woff2');
-}
+const emojiFont = new FontFace("Fluent UI Emoji Flat", "url(assets/font/emoji.woff2)",{
+    display:"swap",
+    style:"normal",
+    weight:"normal"
+});
+const phiFont = new FontFace("phi", `url("${fontPathPhi}2")`,);
+const pageFont = new FontFace("Font", `url("${fontPath}2")`,);
+document.fonts.add(emojiFont);
+document.fonts.add(phiFont);
+document.fonts.add(pageFont);
+emojiFont.load();
+phiFont.load();
+pageFont.load();
 
-`
-const styleEl = document.createElement("style")
-styleEl.innerHTML = style
-document.head.appendChild(styleEl)
-export const fontFaceObservernew = new FontFaceObserver("Font")
-const mFaceObservernew = new FontFaceObserver("Material Icons")
-const eFaceObservernew = new FontFaceObserver("Fluent UI Emoji Flat")
 export const loadFont = async () => {
-    await fontFaceObservernew.load(null, 3000000)
-    await mFaceObservernew.load(null, 3000000)
-    await eFaceObservernew.load(null, 3000000)
+    return await new Promise((r)=>{
+        document.fonts.ready.then(() => {
+            r(null)
+        });
+    })
 }
 
 
